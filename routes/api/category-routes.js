02bca320bc1c8f5918a49ed5178e964901cf8d1d
venasven <honhonlem@gmail.com>
2024-02-09 03:21:55 -0800

    more error fixing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,22 +2,30 @@ const router = require('express').Router();
 const { Category, Product } = require('../../../../../../orm/models');
 
 
-router.get('/', (req, res) => {
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
 router.post('/', (req, res) => {
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
-
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
-
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,25 +1,30 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../../../../../orm/models');
 
-
-router.get('/', (req, res) => {
- 
-});
-
-router.get('/:id', (req, res) => {
- 
-});
-
 router.post('/', (req, res) => {
-
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.put('/:id', (req, res) => {
- 
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.delete('/:id', (req, res) => {
-  
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
